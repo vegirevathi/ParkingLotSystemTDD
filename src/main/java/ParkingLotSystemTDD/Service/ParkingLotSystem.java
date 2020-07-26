681df6119036247d0c73b1dceb6c92d19126bd8c
vegirevathi <vegirevathi@gmail.com>
2020-07-27 00:39:26 +0530

    REFACTORED: Removed unnecessary comments

--- src/main/java/ParkingLotSystemTDD/Service/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystemTDD/Service/ParkingLotSystem.java
@@ -18,7 +18,6 @@ public class ParkingLotSystem {
     }
 
     public boolean unPark(Object vehicle) throws ParkingLotException {
-        //if (this.vehicle == null) return false;
         if (this.vehicle != null && this.vehicle.equals(vehicle)) {
             this.vehicle = null;
             return true;
